695871f34ec038b6a7dc547aaa66d6571550332b
Christian <christianbarrios@ufm.edu>
2022-10-21 09:12:57 -0600

    Build

---
+++ build/precache-manifest.9d76da5b48a112b4fda9c6b272de6b5a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf4d7ef1413498fd370c6ee006ff5926",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b889b499254a224cc7c2",
+    "url": "/static/js/2.db8f847d.chunk.js"
+  },
+  {
+    "revision": "914a85462fc888825cb3a498c2b9869c",
+    "url": "/static/js/2.db8f847d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b64be7d544c08d393b",
+    "url": "/static/js/main.01a20cae.chunk.js"
+  },
+  {
+    "revision": "5dcc4a2d51d1022f96e5",
+    "url": "/static/js/runtime-main.edb82d6c.js"
+  },
+  {
+    "revision": "a44631d4be55cbe3aa7e44e527893fea",
+    "url": "/static/media/Ancelotti.a44631d4.png"
+  },
+  {
+    "revision": "a332f730eaad26f95c49f8b8c469b279",
+    "url": "/static/media/haaland.a332f730.png"
+  },
+  {
+    "revision": "9f94bb6c7782b4db9745322a1363fc0a",
+    "url": "/static/media/profile.9f94bb6c.jpg"
+  },
+  {
+    "revision": "668ff0843fd38f7ce7e377821b38cb6f",
+    "url": "/static/media/svg-2.668ff084.svg"
+  },
+  {
+    "revision": "5df03bb5205ebd02ad9e250149dc642b",
+    "url": "/static/media/svg-3.5df03bb5.svg"
+  }
+]);
