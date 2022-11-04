3d8b0aea3c74c06e8736ed78c01f7dc34a670516
Christian <christianbarrios@ufm.edu>
2022-11-04 15:13:54 -0600

    Nuevas dependencias mysql

--- API-CMK-MYSQL/server.js
+++ API-CMK-MYSQL/server.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const mysql = require ('mysql')
+const myconn = require('express-myconnection')
 
 const app = express()
 app.set('port', process.env.PORT|| 8000)
 
+//middlewares-------------------------------------------
+app.use(myconn(mysql, dbOptions, 'single'))
+
+
+//routes------------------------------------------------
 app.get('/', (req, res)=>{
     res.send('Holi')
 } )
 
+//server running----------------------------------------
 app.listen(app.get('port'), ()=>{
     console.log('server running on port', app.get('port'))
 })
